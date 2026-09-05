4987dc78b920bad48b222d0bd5919585cd9b025e
Lokesh-gudi <lokeshgudi12@gmail.com>
2026-09-05 10:16:04 +0530

    Added Week 4 practice problems

---
+++ Week4-Practice/PayrollDemo.java
@@ -0,0 +1,33 @@
+class Employee {
+    String id;
+    double salary;
+
+    Employee(String id, double salary) {
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void raiseSalary(double salary) {
+        this.salary = this.salary + salary;
+    }
+
+    void printSalary() {
+        System.out.println(id + " | Final Salary: Rs " + salary);
+    }
+}
+
+public class PayrollDemo {
+    public static void main(String[] args) {
+        Employee[] employees = {
+            new Employee("E-101", 40000),
+            new Employee("E-102", 55000),
+            new Employee("E-103", 62000),
+            new Employee("E-104", 48000)
+        };
+
+        for (Employee employee : employees) {
+            employee.raiseSalary(5000);
+            employee.printSalary();
+        }
+    }
+}
